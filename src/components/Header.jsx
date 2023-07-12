261ab4ff11faaaa2b54112343ff02e891f31c997
Jyotendra Singh Chuahan <tezosgecoffcial@gmail.com>
2023-07-12 21:04:03 +0530

    First Commit Tailwind and Header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div className='bg-[black] text-[white]'>
+        <h1>Tezos GEC</h1>
+
+        <ul><li>Home</li>
+        <li>About Us</li>
+        <li>Team</li>
+        <li>Join US</li></ul>
+
+    </div>
+  )
+}
